5ab0ee78dee4fc63e1ee50350dc970de0960b11e
fwsgonzo <fwsgonzo@hotmail.com>
2014-09-26 18:28:58 +0200

    Formatting

--- VideoDirector/src/main/java/io/distributed/videodirector/Server.java
+++ VideoDirector/src/main/java/io/distributed/videodirector/Server.java
@@ -11,41 +11,58 @@ public class Server
 {
     public static void main(String[] args)
     {
-        /*get("/hello",(request, response) ->
-         {
-         return "Hello World!";
-         });*/
         setPort(1234);
-        System.out.println("Testing");
         
-        get("/hello", (request, response) -> {
-            return "Hello World!";
+        get("/conf", 
+        (request, response) ->
+        {
+            if (request.ip().equals("127.0.0.1"))
+            {
+                return "Configuration page";
+            }
+            response.status(401);
+            return "Access denied";
         });
-        post("/hello", (request, response) -> {
+        
+        post("/hello", 
+        (request, response) ->
+        {
             return "Hello World: " + request.body();
         });
-        get("/private", (request, response) -> {
+        
+        get("/private", 
+        (request, response) ->
+        {
             response.status(401);
             return "Go Away!!!";
         });
-        get("/users/:name", (request, response) -> {
+        
+        get("/users/:name", 
+        (request, response) ->
+        {
             return "Selected user: " + request.params(":name");
         });
-        get("/news/:section", (request, response) -> {
+        
+        get("/news/:section", 
+        (request, response) ->
+        {
             response.type("text/html");
             return "<!doctype html><news>" + request.params("section") + "</news>";
         });
-        get("/protected", (request, response) -> {
+        
+        get("/protected", 
+        (request, response) ->
+        {
             halt(403, "I don't think so!!!");
             return null;
         });
-        get("/redirect", (request, response) -> {
+        
+        get("/", 
+        (request, response) ->
+        {
             response.redirect("/news/world");
             return null;
         });
-        get("/", (request, response) -> {
-            return "root";
-        });
-
+        
     }
 }
