d60d63845b6f5c7675eca165e8f5dc240cc62818
fwsgonzo <fwsgonzo@hotmail.com>
2014-10-03 21:13:57 +0200

    Uploading files

--- VideoDirector/src/main/java/io/distributed/videodirector/Server.java
+++ VideoDirector/src/main/java/io/distributed/videodirector/Server.java
@@ -1,6 +1,7 @@
 package io.distributed.videodirector;
 
 import com.google.gson.*;
+import java.io.*;
 import static spark.Spark.*;
 
 /**
@@ -10,6 +11,23 @@ import static spark.Spark.*;
 **/
 public class Server
 {
+    private static int copyInputStream(
+            InputStream  in, 
+            OutputStream out)
+    throws IOException
+    {
+        byte[] buffer = new byte[1024];
+        int len;
+        int total = 0;
+        
+        while ((len = in.read(buffer)) >= 0)
+        {
+            out.write(buffer, 0, len);
+            total += len;
+        }
+        return total;
+    }
+    
     public static void main(String[] args)
     {
         setPort(1234);
@@ -44,7 +62,35 @@ public class Server
             response.type("application/json");
             return new Credentials(user, pass);
         }, 
-        new JsonTransformer());        
+        new JsonTransformer());
+        
+        post("/upload",
+        (request, response) ->
+        {
+            try
+            {
+                InputStream content = request.raw().getInputStream();
+                
+                File file = new File("output.pdf");
+                
+                // if file doesnt exists, then create it
+                //if (!file.exists())
+                //{
+                    file.createNewFile();
+                //}
+                
+                FileOutputStream fw = new FileOutputStream(file.getAbsoluteFile());
+                int len = copyInputStream(content, fw);
+                fw.close();
+                
+                return "Received: " + len + " bytes\n";
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return "Something went wrong";
+        });
         
         get("/private", 
         (request, response) ->
--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl -H "Content-Type: application/json" -d '{"username":"test","password":"123"}' http://localhost:1234/login
+curl --request POST --data-binary "@test.pdf" http://localhost:1234/upload
