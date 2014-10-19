32d419951a2973b8613b65672f110b5eb7692ea5
fwsgonzo <fwsgonzo@hotmail.com>
2014-10-19 03:24:56 +0200

    Proper rank mappings

--- VideoDirector/src/main/java/io/distributed/videodirector/VideoRating.java
+++ VideoDirector/src/main/java/io/distributed/videodirector/VideoRating.java
@@ -32,12 +32,16 @@ import com.google.gson.JsonObject;
 public class VideoRating
 {
     // http://en.wikipedia.org/wiki/Sigmoid_function
-    static double transformed_sigmoid(double x, double scale)
+    static double transformed_sigmoid(double x, double duration)
     {
-        // scale down to sane range
-        x /= scale;
+        // common video duration mapping
+        x *= x / duration;
+        x -= 0.1 * duration;
+
         // some arbitrary sigmoid function
         double f = x / (1 + Math.abs(x));
+        f = Math.pow(f, 3.0);
+
         // transform from [-1, 1] to [0, 1]
         return (f + 1.0) * 0.5;
     }
@@ -56,45 +60,37 @@ public class VideoRating
         };
         final double weights[] = 
         {
-            0.55, // shake
-            0.45  // tilt
+            0.60, // shake
+            0.40  // tilt
         };
         
         double dot = 0.0f;
-        for (int i = 0; i < kernel.length; i++)
+        for (int i = 0; i < 2; i++)
         {
-            dot += Math.pow(kernel[i] * weights[i], 2.0);
+            dot += (kernel[i] * kernel[i]) * weights[i];
         }
         double root = Math.sqrt(dot);
         
         return (int) (root * 100.0);
     }
     
-    // rates a video according to normalization functions
-    // TODO: currently only using tanh as normalizer, replace with better
     /**
      * @param obj Json string of video to be rated
      * @return Returns the rating for a video in range [0, 100]
     **/
     static int rate(JsonObject obj)
     {
-        // timestamp finish_time
-        // int duration
-        // int width
-        // int height
-        // int shaking
-        // int tilt
         double dur   = obj.get("duration").getAsInt() / 1000.0;
         if (dur < 0.1) dur = 0.1; // clamp to some positive
         
         int tilt  = obj.get("tilt").getAsInt();
         int shake = obj.get("shaking").getAsInt();
         
-        // apply sigmoid function for mapping to [0, 1]
-        double nshake = transformed_sigmoid(shake, 1.5 * dur);
-        double ntilt  = transformed_sigmoid(tilt, 1.5 * dur);
+        // transformation function: f(duration, shake/tilt)
+        double nshake = transformed_sigmoid(shake, dur);
+        double ntilt  = transformed_sigmoid(tilt,  dur);
         
-        // given that most mobile videos are low w:h does it even matter?
+        // apply final ranking kernel for weighting shake vs tilt
         return rank(nshake, ntilt);
     }
 }
